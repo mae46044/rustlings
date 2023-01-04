c8b62d26aaefcda41154bd16c39325a11a9da9b5
mae46044 <mae46044@gmail.com>
2023-01-04 13:07:18 +0900

    ver. 20230104.

--- exercises/traits/traits3.rs
+++ exercises/traits/traits3.rs
@@ -7,10 +7,18 @@
 // Consider what you can add to the Licensed trait.
 // Execute `rustlings hint traits3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 pub trait Licensed {
     fn licensing_info(&self) -> String;
+}
+
+impl Licensed for dyn Software {
+    fn licensing_info(&self) -> String {
+        format!("{}", self.version())
+    }
+}
+
+pub trait Software {
+    fn version(self) -> String;
 }
 
 struct SomeSoftware {
@@ -19,6 +27,18 @@ struct SomeSoftware {
 
 struct OtherSoftware {
     version_number: String,
+}
+
+impl Software for SomeSoftware {
+    fn version(self) -> String {
+        format!("{}", self.version_number)
+    }
+}
+
+impl Software for OtherSoftware {
+    fn version(self) -> String {
+        format!("{}", self.version_number)
+    }
 }
 
 impl Licensed for SomeSoftware {} // Don't edit this line
